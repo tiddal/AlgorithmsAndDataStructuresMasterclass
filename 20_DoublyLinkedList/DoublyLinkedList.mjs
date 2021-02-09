754bd61fec5c260fd49ea1f3b74f9e7327d00837
tiddal <tiddal96@gmail.com>
2021-02-09 10:25:37 +0000

    Doubly linked list

--- 20_DoublyLinkedList/DoublyLinkedList.mjs
+++ 20_DoublyLinkedList/DoublyLinkedList.mjs
@@ -1,7 +1,6 @@
-import { Node } from "./Node.mjs";
+import { Node } from './Node.mjs';
 
-class SinglyLinkedList {
-
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -10,53 +9,52 @@ class SinglyLinkedList {
 
   push(val) {
     const newNode = new Node(val);
-    if (!this.head) this.head = newNode, this.tail = newNode;
-    else this.tail.next = newNode, this.tail = newNode;
+    if (!this.head) this.head = newNode;
+    else this.tail.next = newNode, newNode.previous = this.tail;
+    this.tail = newNode;
     this.length++;
     return this;
   }
 
   pop() {
     if (!this.head) return undefined;
-    let current = this.head;
-    let newTail = current;
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-    this.tail = newTail;
+    const removedItem = this.tail;
+    this.tail = this.tail.previous;
     this.tail.next = null;
     this.length--;
     if (this.length === 0) this.head = null, this.tail = null;
-    return current.val;
+    return removedItem.val;
   }
 
   shift() {
     if (!this.head) return undefined;
-    let oldHead = this.head;
-    this.head = oldHead.next;
+    const oldHead = this.head;
     this.length--;
-    if (this.length === 0) this.tail = null;
+    if (this.length === 0) { this.head = null, this.tail = null; return oldHead.val; }
+    this.head = this.head.next;
+    this.head.previous = null;
     return oldHead.val;
   }
 
   unshift(val) {
     if (!this.head) return this.push(val);
-    let oldHead = this.head;
-    const newNode = new Node(val);
-    newNode.next = oldHead;
-    this.head = newNode;
+    const newHead = new Node(val);
+    this.head.previous = newHead;
+    newHead.next = this.head;
+    this.head = newHead;
     this.length++;
     return this;
   }
 
   get(idx) {
     if (idx < 0 || idx >= this.length) return null;
-    let counter = 0;
-    let current = this.head;
-    while (counter !== idx) {
-      current = current.next;
-      counter++;
+    let counter, current;
+    if (idx <= this.length / 2) {
+      counter = 0, current = this.head;
+      while (counter !== idx) current = current.next, counter++;
+    } else {
+      counter = this.length - 1, current = this.tail;
+      while (counter !== idx) current = current.previous, counter--;
     }
     return current;
   }
@@ -75,6 +73,8 @@ class SinglyLinkedList {
     const previousNode = this.get(idx - 1);
     const newNode = new Node(val);
     newNode.next = previousNode.next;
+    newNode.next.previous = newNode;
+    newNode.previous = previousNode;
     previousNode.next = newNode;
     this.length++;
     return true;
@@ -87,6 +87,7 @@ class SinglyLinkedList {
     const previousNode = this.get(idx - 1);
     const removedNode = previousNode.next;
     previousNode.next = removedNode.next;
+    removedNode.next.previous = previousNode;
     this.length--;
     return removedNode.val;
   }
@@ -95,19 +96,22 @@ class SinglyLinkedList {
     let node = this.head;
     this.head = this.tail;
     this.tail = node;
-    let nextNode;
-    let previousNode = null;
-
     for (let i = 0; i < this.length; i++) {
-      nextNode = node.next;
-      node.next = previousNode;
-      previousNode = node;
-      node = nextNode;
+      let previousNode = node.next;
+      node.next = node.previous;
+      node = previousNode;
     }
-
     return this;
   }
-
 }
 
-export { SinglyLinkedList };
+let dl = new DoublyLinkedList;
+dl.push(10).push(15).push(20);
+dl.reverse();
+console.log(dl.length);
+console.log(dl.head.val);
+
+
+
+
+export { DoublyLinkedList };
---
+++ 20_DoublyLinkedList/Node.mjs
@@ -0,0 +1,9 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+export { Node };
